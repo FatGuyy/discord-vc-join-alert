202fe8bfcbf264563a608c64826895e73b56b9cc
FatGuyy <omkarjadhav2103@gmail.com>
2022-09-04 14:09:11 +0530

    Done with scraping info, But taking too much time to load server info

--- vc_members.py
+++ vc_members.py
@@ -29,44 +29,57 @@ except TimeoutException:
 #test = driver.find_element(by=By.XPATH,value="//div [@class='iconContainer-21RCa3']") #iconContainer-21RCa3
  
 voice_channels = []
+vc_names=[]
 channels = driver.find_elements(By.XPATH, value='/html/body/div[1]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/nav/div[4]/ul/li')
 limit = len(channels)
-channels= []
-for i in range(limit): 
-    channels.append(driver.find_elements(By.XPATH, value=f'/html/body/div[1]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/nav/div[4]/ul/li{i}/div/div/a/div[1]'))
-    print('here.')
+print(channels , '\n\n', limit)
+channels=[]
+
+for indx in range(1, limit+2):
+    try:
+        channels.append(driver.find_element(By.XPATH,value=f'/html/body/div[1]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/nav/div[4]/ul/li[{indx}]/div/div/div/a'))
+    except:
+        continue
+
+print(len(channels))
+
+#General (voice channel), 1 user
+#res = test_string.partition(spl_word)[0]
 for channel in channels:
-    if channel.get_attribute('aria-label') == 'Voice':
-        voice_channels.append(channel)   
-        print('added.') 
+    aria_label = channel.get_attribute('aria-label')
+    if "(voice channel)" in aria_label:
+        name = str(aria_label).partition('(voice channel)')[0]
+        vc_names.append(name)
+        voice_channels.append(channel)
+
+print('v channels : ', voice_channels, '\n\n','vc names :', vc_names, '\n\n')
+# for indx in range(1,limit+2): 
+#         channels.append(driver.find_element(By.XPATH, value=f'/html/body/div[1]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/nav/div[4]/ul/li[{indx}]/div[1]/div/div/a'))
+#         print('here.')
+
+# for channel in channels:
+#     if channel.get_attribute('aria-label') == 'Voice':
+#         voice_channels.append(channel)   
+#         print('added.') 
 
 # for indx,channel in enumerate(channels):
 #     if channel.get_attribute('aria-label') == 'Voice':
 #         voice_channels.append(indx)
-print(voice_channels)
 
 
-for indx in voice_channels:
-    peoples = driver.find_elements(by=By.XPATH,value=f"/html/body/div[1]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/nav/div[4]/ul/li[{indx}]/div[2]/div[1]/div/div/div")
-    print(indx)
-    for people in peoples:
-        name = people.text   
-        print(name)
 
-
-# print(voice_channels)
-# print(voice_channels, 'printing voice channels as elements ')
-
-
-#for channel in voice_channels:
-# for i in range(len(voice_channels)):
-    # voice_channels_names.append(driver.find_element(by=By.XPATH, value="//div [@class='iconContainer-21RCa3']").text)
-
+# for indx in voice_channels:
+#     peoples = driver.find_elements(by=By.XPATH,value=f"/html/body/div[1]/div[2]/div/div[1]/div/div[2]/div/div[1]/div/div/div[1]/nav/div[4]/ul/li[{indx}]/div[2]/div[1]/div/div/div")
+#     print(indx)
+#     for people in peoples:
+#         name = people.text   
+#         print(name)
 
 
 if __name__ == '__main__':
-    for i in range(len(vc_members)):
-        print(vc_members[i].text)
-    time.sleep(5)
+    pass
+    # for i in range(len(vc_members)):
+    #     print(vc_members[i].text)
+    # time.sleep(5)
 
 driver.close()
